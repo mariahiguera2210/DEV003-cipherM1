7c437108431f00438a6bb99a620a2164a18a34cf
mariahiguera2210 <angelica.higueram92@gmail.com>
2022-11-28 23:40:28 -0500

    guardar avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import cipher from './cipher.js';
 
-console.log(cipher);
+
+function miMensaje() {
+
+    let mensaje = document.getElementById("texto").value
+    let seleccionado = document.querySelector('input[name="mensaje"]:checked').value;
+    
+    let respuesta;
+
+    if(seleccionado == "Encriptar" ) 
+        respuesta = "formula para encriptar el mensaje"
+
+    else if(seleccionado == "Desencriptar")
+        respuesta = "formula para desencriptar el mensaje"
+
+
+    
+
+    
+
+    
+
+
+
+
+
+}
