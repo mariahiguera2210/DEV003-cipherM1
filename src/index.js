5fe3baf2c24f62d5ccfae5ded061ea71d7f98047
mariahiguera2210 <angelica.higueram92@gmail.com>
2022-11-29 13:17:55 -0500

    avance martes 29/11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import cipher from './cipher.js';
+
+console.log(cipher);
 
 
 function miMensaje() {
 
     let mensaje = document.getElementById("texto").value
     let seleccionado = document.querySelector('input[name="mensaje"]:checked').value;
-    
     let respuesta;
+    let desplazamientos= document.getElementById('ndesplazamiento').value;
+    console.log(mensaje, seleccionado, desplazamientos)
 
     if(seleccionado == "Encriptar" ) 
         respuesta = "formula para encriptar el mensaje"
@@ -16,12 +20,8 @@ function miMensaje() {
 
     
 
-    
-
-    
-
-
-
-
-
 }
+
+
+let boton= document.getElementById('enviar');
+boton.addEventListener("click",miMensaje);
